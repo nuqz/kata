6f60b7261c58432fd661cfa3a30af9b8bd1f5da7
Nuqz <nuqz@indiesoft.ru>
2024-03-15 18:37:59 +0300

    Remove unused variable

--- utils.go
+++ utils.go
@@ -27,28 +27,6 @@ var (
 		"IX":   9,
 		"X":    10,
 	}
-	RomansByArabic = map[int]string{
-		1:  "I",
-		2:  "II",
-		3:  "III",
-		4:  "IV",
-		5:  "V",
-		6:  "VI",
-		7:  "VII",
-		8:  "VIII",
-		9:  "IX",
-		10: "X",
-		11: "XI",
-		12: "XII",
-		13: "XIII",
-		14: "XIV",
-		15: "XV",
-		16: "XVI",
-		17: "XVII",
-		18: "XVIII",
-		19: "IXX",
-		20: "XX",
-	}
 )
 
 type Operand struct {
